ae57826ad363fda96501e97ebc78b502255cafd1
LalKrishnan <krishnanpraveen099@gmail.com>
2022-10-09 11:08:43 +0530

    addeventListener mousemove

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import React,{ useEffect, useState} from 'react'
 import { Hello } from './Hello';
 import { useForm } from './useForm'
 
-
+// usecases of useEffect mount , unmount in regular app
 const App = () => {
   const [showHello,setShowHello] = useState(true)
   const [values, setValues ] = useForm({email:'', password: '', firstname:''});
 
     // cool thing about useEffect it doesn't just have to work when a component mount ,unmount  // meaning
   useEffect(()=>{
-    console.log('render');
+    const onMousemove = e => {
+      console.log(e);
+    }
+    // Effects are a great place to addevent listeners and remove them
+    window.addEventListener('mousemove',onMousemove)
 
     return ()=>{
-      console.log('unmount');
+      window.removeEventListener('mousemove',onMousemove)
     } // lets say we want this to fire when ever values.email changes ; meaning its gonna call return() i.e the cleanup function \
       // so every time we change the values.email its gonna unmount , /its not really unmounting anything / its just cleaning up the old value 
       // in this case the old values.email
       // and we have a new value on the next render 
-  },[values.email])
+  },[])
 
   // 
 
