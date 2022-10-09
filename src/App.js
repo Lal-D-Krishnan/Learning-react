ff1a6fa3e5bb91d0a55ff829dc17a8f12ad54a00
LalKrishnan <krishnanpraveen099@gmail.com>
2022-10-09 10:40:08 +0530

    Proper working of mount unmount in useeffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 // learning useEffect from ben awad - React hook tutorial
 import React,{ useEffect, useState} from 'react'
+import { Hello } from './Hello';
 import { useForm } from './useForm'
 
 
 const App = () => {
-  const [values, setValues ] = useForm({email:'', password: ''});
+  const [showHello,setShowHello] = useState(true)
+  const [values, setValues ] = useForm({email:'', password: '', firstname:''});
 
-  useEffect(()=>{
-    console.log('render');
-  })
+  // useEffect(()=>{
+  //   console.log('render');
+
+  //   return ()=>{
+  //     console.log('unmount');
+  //   }
+  // },[])
 
   return (
       <div>
-        
+        <button onClick={()=> setShowHello(!showHello)} >toggle</button>
+          {showHello &&  <Hello/>}
            <input type="text" name='email' value={values.email} onChange={setValues} />
+           <input type="text" name='firstname' value={values.firstname} onChange={setValues} placeholder='firstname' />
            <input type="password" name='password' value={values.password} onChange={setValues} />
 
       </div>
